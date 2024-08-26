198b28d1549695ed3b5b8707d4f796ae55b012dd
Alex Faktor <118138704+AlexFaktor@users.noreply.github.com>
2024-08-26 14:35:15 +0300

    project structure | refactoring

--- Game.Discord/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App.Discord;
-
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
--- Game.GameCore/Manager/GameManager.cs
+++ Game.GameCore/Manager/GameManager.cs
@@ -5,6 +5,26 @@ namespace App.GameCore.Manager;
 public class GameManager : IGameManager
 {
     private GameConfig _config;
+
+    public void MakeBattle(BattleConfiguration configuration, BattleRewards rewards)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MakeChoice()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MakeGacha()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetSquad()
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public interface IGameManager
